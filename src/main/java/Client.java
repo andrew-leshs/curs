55e55936ac3f5e77f0cbdee65fc66e1c3c1c21a1
Your Name <you@example.com>
2022-10-07 15:08:13 +0300

    test commit

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,4 +1,6 @@
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.net.Socket;
@@ -6,10 +8,10 @@ import java.net.Socket;
 public class Client implements Serializable{
     public static final long serialVersionUID = 42L;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
         try (Socket clientSocket = new Socket("127.0.0.1", 8989);
              ObjectOutputStream objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
-             ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream())
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
         ) {
 
             JSONObject root = new JSONObject();
@@ -18,10 +20,10 @@ public class Client implements Serializable{
             root.put("sum", 200);
 
             objectOutputStream.writeObject(root);
-            root = (JSONObject) in.readObject();
-            System.out.println(root);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            String s = in.readLine();
+            JSONParser parser = new JSONParser();
+            JSONObject json = (JSONObject) parser.parse(s);
+            System.out.println(json);
         }
     }
 }
